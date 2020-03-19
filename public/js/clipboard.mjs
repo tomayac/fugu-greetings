e3969067af2226bcb68bda4b77ff1923efcc8462
tomayac <steiner.thomas@gmail.com>
2020-03-19 18:24:36 +0100

    Improve clipboard code, simplify export code

--- public/js/clipboard.mjs
+++ public/js/clipboard.mjs
@@ -16,13 +16,9 @@ const paste = async () => {
   try {
     const clipboardItems = await navigator.clipboard.read();
     for (const clipboardItem of clipboardItems) {
-      try {
-        for (const type of clipboardItem.types) {
-          const blob = await clipboardItem.getType(type);
-          return blob;
-        }
-      } catch (err) {
-        console.error(err.name, err.message);
+      for (const type of clipboardItem.types) {
+        const blob = await clipboardItem.getType(type);
+        return blob;
       }
     }
   } catch (err) {
--- public/js/export_image_legacy.mjs
+++ public/js/export_image_legacy.mjs
@@ -3,7 +3,6 @@ import {exportButton, toBlob} from './script.mjs';
 export const exportImage = async (blob) => {
   const a = document.createElement('a');
   a.download = 'fugu-greeting.png';
-  a.rel = 'noopener';
   a.href = URL.createObjectURL(blob);
   a.addEventListener('click', (e) => {
     a.remove();
