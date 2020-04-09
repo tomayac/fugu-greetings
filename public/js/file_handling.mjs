0571e909cf1929488d0d92cfd1c83bc30ed487b2
tomayac <steiner.thomas@gmail.com>
2020-04-09 23:55:26 +0200

    File handling

--- public/js/file_handling.mjs
+++ public/js/file_handling.mjs
@@ -1,10 +1,11 @@
 import {drawBlob} from './script.mjs';
 
 const handleLaunchFiles = () => {
-  console.log(window.launchParams);
-  window.launchParams.files.forEach(async (handle) => {
-    const file = await handle.getFile();
-    drawBlob(file);
+  window.launchQueue.setConsumer((launchParams) => {
+    launchParams.files.forEach(async (handle) => {
+      const file = await handle.getFile();
+      drawBlob(file);
+    });
   });
 };
 
