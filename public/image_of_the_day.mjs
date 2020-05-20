f42e4188622ce9bfa7a07a94f2d1c1604c10158b
Hemanth HM <hemanth.hm@gmail.com>
2020-05-20 16:29:36 -0700

    using unsplash for image of the day, fixes #11

--- public/image_of_the_day.mjs
+++ public/image_of_the_day.mjs
@@ -1,23 +1,10 @@
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-
 // eslint-disable-next-line no-unused-vars
 const getImageOfTheDay = async () => {
   let url = './assets/default_background.jpg';
   try {
-    let response = await fetch(`${CORS_PROXY}https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss`);
-    if (!response.ok) {
-      throw new Error('Response was', response.status, response.statusText);
-    }
-    const xmlString = await response.text();
-    // Parsing XML with RegExp, because neither DOMParser nor XMLHttpRequest
-    // are exposed in the service worker context. Also: YOLO… 😎
-    url = xmlString.match(/<enclosure\s+url="([^"]+)/m)[1];
-
-    response = await fetch(`${CORS_PROXY}${url}`);
-    if (!response.ok) {
-      throw new Error('Response was', response.status, response.statusText);
-    }
-    return await response.blob();
+    let fishes = "blowfish, pufferfish, fugu".split(",");
+    let fish = [~~(fishes.length * Math.random())];
+    return await (await fetch(`https://source.unsplash.com/daily?${fish}`)).blob()
   } catch (err) {
     console.error(err.name, err.message);
   }
